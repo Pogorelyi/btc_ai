7b1777e6a2d89ce70c881ce8da9f111bea23f7ea
Dima Pogorelyi <pogorelyi@together.com>
2019-08-16 01:40:42 +0300

    s

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-import numpy as nm
-from config.config import get_config
+from bitmex_websocket import BitMEXWebsocket
+from time import sleep
 
-print(nm.array([1, 2, 3]))
+end_point = "https://testnet.bitmex.com/api/v1"
+api_key = 'rMnq_8rhjmuuw2O4WBIiA8Bj'
+api_secret = '7s4RiqCKV9e1fggCJEmO87JRZiVvOr8RjDFNMH7BINb0UQVm'
+sleep_seconds = 4
 
-print (get_config())
+
+def get_price(ws):
+    ws.get_instrument()
+    ticker = ws.get_ticker()
+
+    return ticker['last']
+    # while (ws.ws.sock.connected):
+    #     ticker = ws.get_ticker()
+    #     print(ticker['last'])
+    #     sleep(sleep_seconds)
+
+
+def get_orders(ws):
+    orders = ws.funds()
+    print(orders)
+
+
+if __name__ == "__main__":
+    ws = BitMEXWebsocket(endpoint=end_point, symbol="XBTUSD", api_key=api_key, api_secret=api_secret)
+    while 1:
+        method = int(input("1. price | 2. orders\n"))
+        if method == 1:
+            print(get_price(ws))
+        else:
+            get_orders(ws)
+
